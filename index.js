ebbb0276aece7c4f4f782ebd0004825a605ba4bf
Joseph Wachira <washywachira@gmail.com>
2023-03-18 11:59:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = [
+    { name: 'John', hometown: 'New York' },
+    { name: 'Mike', hometown: 'Los Angeles' },
+    { name: 'Sara', hometown: 'Chicago' },
+    { name: 'Adam', hometown: 'San Francisco' },
+    { name: 'Emily', hometown: 'Boston' }
+  ];
+  
+  function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
